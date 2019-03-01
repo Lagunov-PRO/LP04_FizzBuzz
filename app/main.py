ae594a8dd484d10cd9c4adecc8e030f6d7252577
Lagunov-PRO-Work <alexey@lagunov.pro>
2019-03-01 12:25:58 +0300

    First attempt. Fail.

--- app/main.py
+++ app/main.py
@@ -1,2 +1,9 @@
 for number in range(1, 101):
-    print(number)
+    # if number % 3 == 0 and number % 5 == 0:
+    #     print('FizzBuzz')
+    if number % 3 == 0:
+        print('Fizz', end='' if number % 5 == 0 else '\n')
+    if number % 5 == 0:
+        print('Buzz')
+    else:
+        print(number)
