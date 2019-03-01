a4ddd7519030cf370c2fd17c10f3f05638ccb33a
Lagunov-PRO-Work <alexey@lagunov.pro>
2019-03-01 13:13:52 +0300

    Added default values to the dictionary.

--- app/lib.py
+++ app/lib.py
@@ -1,6 +1,7 @@
-def fizzbuzz(substitutes: dict, upper_limit=100):
+def fizzbuzz(substitutes={'Fizz': 3, 'Buzz': 5}, upper_limit=100):
     """
-    >>> fizzbuzz({'Fizz' : 3, 'Buzz' : 5}, 20)
+    Replaces the numbers divisible by 3 with Fizz, by 5 with Buzz, with both by FizzBuzz and so on...
+    >>> fizzbuzz({'Fizz': 3, 'Buzz': 5}, 20)
     [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz', 16, 17, 'Fizz', 19, 'Buzz']
 
     >>> fizzbuzz({'Fizz': 3, 'Buzz': 5, 'Fuzz': 7}, 200)[100:110]
--- app/main.py
+++ app/main.py
@@ -1,5 +1,3 @@
 from app.lib import fizzbuzz
 
-print(fizzbuzz({'Fizz': 3, 'Buzz': 5, 'Fuzz': 7}, 200)[100:110])
-
-#  Bizz, Biff
+print(fizzbuzz())
