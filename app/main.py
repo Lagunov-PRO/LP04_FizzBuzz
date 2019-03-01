4cf9f47cb56baecf38de5fd3e1d297a8961d79da
Lagunov-PRO-Work <alexey@lagunov.pro>
2019-03-01 12:29:21 +0300

    Third attempt. Optimized.

--- app/main.py
+++ app/main.py
@@ -1,5 +1,3 @@
 for number in range(1, 101):
-    if number % 3 == 0 or number % 5 == 0:
-        print('{}{}'.format('Fizz' if number % 3 == 0 else '', 'Buzz' if number % 5 == 0 else ''))
-    else:
-        print(number)
+    fizzbuzz = ('{}{}'.format('Fizz' if number % 3 == 0 else '', 'Buzz' if number % 5 == 0 else ''))
+    print(number if not fizzbuzz else fizzbuzz)
