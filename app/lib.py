8ca4633825960f95a73c9b0da1137c381d48fd18
Lagunov-PRO-Work <alexey@lagunov.pro>
2019-03-01 13:34:09 +0300

    Shortened the doctests.

--- app/lib.py
+++ app/lib.py
@@ -1,8 +1,12 @@
 def fizzbuzz(substitutes={'Fizz': 3, 'Buzz': 5}, upper_limit=100):
     """
-    Replaces the numbers divisible by 3 with Fizz, by 5 with Buzz, with both by FizzBuzz and so on...
-    >>> fizzbuzz({'Fizz': 3, 'Buzz': 5}, 20)
-    [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz', 16, 17, 'Fizz', 19, 'Buzz']
+    Replaces the numbers divisible
+        by 3 with Fizz
+        by 5 with Buzz
+        by both by FizzBuzz
+        and so on...
+    >>> fizzbuzz({'Fizz': 3, 'Buzz': 5}, 15)[5:]
+    ['Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz']
 
     >>> fizzbuzz({'Fizz': 3, 'Buzz': 5, 'Fuzz': 7}, 200)[100:110]
     [101, 'Fizz', 103, 104, 'FizzBuzzFuzz', 106, 107, 'Fizz', 109, 'Buzz']
--- app/main.py
+++ app/main.py
@@ -1,3 +1,3 @@
 from app.lib import fizzbuzz
 
-print(fizzbuzz())
+print(fizzbuzz({'Fizz': 3, 'Buzz': 5}, 15)[5:])
